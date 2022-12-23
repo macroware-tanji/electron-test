1cbc116e89d6e0b356a67e0fc379b31a75d5432a
tanji <tanji@macroware.co.jp>
2022-12-23 10:11:11 +0900

    キャプチャ機能修正

--- src/renderer.js
+++ src/renderer.js
@@ -40,35 +40,34 @@ async function startCapture() {
       //}
       names += source.name
       console.log(source.thumbnail.toDataURL())
-    //   var thumbnailImage = new Image(200, 200);
-    //   thumbnailImage.src = source.thumbnail.toDataURL();
-    //   thumbnailElem.appendChild(thumbnailImage);
-    thumbnailElem.innerHTML += `<img src="${source.thumbnail.toDataURL()}"/><br>`;
+      //   var thumbnailImage = new Image(200, 200);
+      //   thumbnailImage.src = source.thumbnail.toDataURL();
+      //   thumbnailElem.appendChild(thumbnailImage);
+      thumbnailElem.innerHTML += `<img src="${source.thumbnail.toDataURL()}"/><br>`;
 
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: sources[0].id,
-              minWidth: 1280,
-              maxWidth: 1280,
-              minHeight: 720,
-              maxHeight: 720
-            }
-          }
-        })
-        handleStream(stream)
-      } catch (e) {
-        handleError(e)
-      }
 
     }
-    
-
     console.log(names)
-    // try {
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: sources[0].id,
+            minWidth: 1280,
+            maxWidth: 1280,
+            minHeight: 720,
+            maxHeight: 720
+          }
+        }
+      })
+      handleStream(stream)
+    } catch (e) {
+      handleError(e)
+    }
+  // try {
     //   videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
     //   dumpOptionsInfo();
     // } catch (err) {
